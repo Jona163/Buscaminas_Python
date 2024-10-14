ce4fb2839e275321f22b4fcceb1ca232eea9c5ac
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-13 20:24:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 # Fecha: 13 octubre 2024
 # Descripción: Buscaminas en Python.
 # GitHub: https://github.com/Jona163
+
+from random import randint
+
+# Juego de Buscaminas desarrollado en Python.
+# Crear matriz, esta función no invoca ninguna otra función del programa.
+def matriz(filas, columnas, caracter=False):
+    tablero = []
+    for i in range(filas):
+        v = [caracter] * columnas
+        tablero.append(v)
+    return tablero
